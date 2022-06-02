85f9a81faf0cf7950b5c6e2c5b224988e6282c96
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-02 15:22:43 -0700

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* main function prints some strings*/
+/**
+ * main - print where we begin
+ * Return:  return zero to declare sucess
+ */
+int main(void)
+{
+	putcahr("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
