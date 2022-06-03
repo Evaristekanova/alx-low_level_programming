4370ba5177f6468a33d5bf5a874206c6aa31f80a
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-03 16:36:25 -0700

    single digits using putcar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-printing alphabet in lowwercase
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
