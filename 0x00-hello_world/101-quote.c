807296c758a08e7dd6d69ff9728eb4f7ffc47478
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-03 03:07:36 -0700

    rename default

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /* main function prints some strings*/
 /**
  * main - print where we begin
- * Return:  return zero to declare sucess
+ * Return: 1 if success sucess
  */
 int main(void)
 {
-	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
+	putchar("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
