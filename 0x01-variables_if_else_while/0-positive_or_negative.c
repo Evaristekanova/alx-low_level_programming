19d0b752a43bbac2ddd3644ec6a24b6bd754dd8d
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-03 14:00:36 -0700

    chacking if number is positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,12 +15,12 @@ int main(void)
 			printf("%d is positive\n", n);
 		}
 		n = -98;
-		else if (n < 0)
+		if (n < 0)
 		{
 			printf("%d is negative\n", n);
 		}
 		n = 0;
-		else
+		if (n == 0)
 		{
 			printf("%d is zero\n", n);
 		}
