789380e1b7564dde4e0585c88e1866b1acf42bdb
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-03 16:27:29 -0700

    displaying single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-printing single digit from 0 to 10
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	return (0);
+}
