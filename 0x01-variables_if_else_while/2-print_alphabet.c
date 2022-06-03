d5d491b2471e139179352cd74a24fc9d4c891c0f
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-03 15:07:19 -0700

    displaying lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-printing alphabet in lowwercase
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	char chara;
+
+	for (chara = 'a'; chara <= 'z'; chara++)
+	{
+		putchar(chara);
+	}
+	return (0);
+}
