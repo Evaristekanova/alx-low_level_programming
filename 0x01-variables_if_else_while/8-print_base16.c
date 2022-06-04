a7462c0ea7d244169aaef564b826ead2e778ec32
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-04 01:26:36 -0700

    printing number in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-printing alphabet in lowwercase
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num <= 10)
+		{
+			for (num = 'a'; num <= 'f'; num++)
+			{
+				putchar(num);
+			}
+		}
+		putchar(num);
+	}
+	return (0);
+}
