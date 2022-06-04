e2a300aceb26f8b9f4f27b89e70b44e0765f3432
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-04 03:35:24 -0700

    displaying last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-printing alphabet in lowwercase
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(n));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf(" The last digit of %d is %d and greater than 5", n, (n % 10));
+	}
+
