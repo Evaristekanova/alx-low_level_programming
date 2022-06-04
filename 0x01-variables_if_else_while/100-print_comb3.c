4f836f59ccccdede9df077a4264d5a3d624e7aef
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-04 02:25:18 -0700

    combination of two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-printing alphabet in lowwercase
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 0 ; j <= 9; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
