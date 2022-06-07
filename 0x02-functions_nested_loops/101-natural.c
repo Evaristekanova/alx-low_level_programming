8ffe65f96aab4a0929371674d1de828ac21844d1
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-07 12:47:34 -0700

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-sum of number which is multiple os 3 and 5
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i / 3 == 0) || (i / 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
+
