97da956f97004c9a8c5c40daf5c733120c7eeecd
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-07 10:36:13 -0700

    chacking if number is positive

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _islower-checking if letter is lowercaese or uppercase
+ *
+ * Return:value
+ */
+int _islower(int c)
+{
+
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign-printing sign of number
+ *
+ * @n:argument for the function
+ * Return:value
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
+
