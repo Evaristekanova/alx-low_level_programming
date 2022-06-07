f8fd82dae4756cffe78dcedc6cb871b09482472a
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-07 11:00:26 -0700

    absolute value of a given number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,13 @@
 /**
  * _abs-printing absolute value of any number
  *
- * @:parameter for the function
+ * @n:parameter for the function
  * Return:0 if succcess
  */
 int _abs(int n)
 {
 	int m;
+
 	m = abs(n);
 	return (m);
 }
