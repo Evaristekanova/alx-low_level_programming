a7054ad5b237a6e09898fbf413e46378360a2a3f
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-07 09:48:04 -0700

    displaying _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,8 @@
 int main(void)
 {
 	int i = 0;
-	char str[8]={'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	char str[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
 	while (i < 8)
 	{
 		_putchar(str[i]);
