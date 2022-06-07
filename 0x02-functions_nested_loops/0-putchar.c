cde1d4d7ef96b6c9fab58f726ae85e44af6b24d9
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-07 09:45:30 -0700

    displaying _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main-printing some strings
+ *
+ * Return:o if success
+ */
+int main(void)
+{
+	int i = 0;
+	char str[8]={'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	while (i < 8)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
