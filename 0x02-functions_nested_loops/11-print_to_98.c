00e1047dcf50eab352feb8e33a99a96085d249a0
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-07 12:07:30 -0700

    print numbers wuthin range

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,23 +11,29 @@ void print_to_98(int n)
 	int i;
 	int j;
 
-	for (i = n; i <= 98; i++)
+	if (i < 98)
 	{
-		if (i == 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			printf("%d,", i);
+			printf(" ");
 		}
-		printf("%d,", i);
-		printf(" ");
 	}
 
-	for (j = n; j >= 98; j--)
+	else
 	{
-		if (j == 98)
+		for (j = n; j >= 98; j--)
 		{
-			printf("%d", j);
+			if (j == 98)
+			{
+				printf("%d", j);
+			}
+			printf("%d,", j);
+			printf(" ");
 		}
-		printf("%d,", j);
-		printf(" ");
 	}
 }
