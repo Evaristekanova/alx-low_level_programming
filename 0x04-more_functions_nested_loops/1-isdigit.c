ad9c07faf20d8f47db3ef0b515af151add81d80c
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-09 10:41:28 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
  * _isdigit-printing numbers
  *
  * @c:parameter of the function
+ *
  * Return:value
  */
+
 int _isdigit(int c)
 {
+
 	if (c <= 48 && c >= 57)
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
 	}
+
 }
