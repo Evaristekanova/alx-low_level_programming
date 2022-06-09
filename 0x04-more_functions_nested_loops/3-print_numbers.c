ce54f3f4e31af1505bae374b4bbe1ddc9a174528
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-09 08:08:50 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers-function to print number btn 0 and 10
+ *
+ * Return:0 if success
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
