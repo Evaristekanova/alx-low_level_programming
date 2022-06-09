59c9b05bf0bc7c3b13479173903412fd3a413246
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-09 07:33:03 -0700

    if character is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit-cheching if letter is digit
+ *
+ * @c:parameter for the function
+ * Return:0 if success
+ */
+int _isdigit(int c)
+{
+	if (c <= 48 && c >= 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
