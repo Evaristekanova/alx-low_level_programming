331f2ee88a476fa67e6506f800949e5b7c7fad56
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-09 10:35:57 -0700

    upper case

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,18 @@
-
+#include "main.h"
+/**
+ * _isdigit-printing numbers
+ *
+ * @c:parameter of the function
+ * Return:value
+ */
+int _isdigit(int c)
+{
+	if (c <= 65 && c >= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
