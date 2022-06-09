080352e774343d67bee2290f05baed019fe539bf
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-09 06:58:09 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MATH_H
+/**
+ * _patchar-printing using putchar function
+ *
+ * @c:parameter for the user defined function
+ *Return:0 if success
+ */
+ int _isupper(int c);
+ int _isdigit(int c);
+ int mul(int a, int b);
+ void print_numbers(void);
+ void print_most_numbers(void);
+ void more_numbers(void);
+ void print_line(int n);
+ void print_diagonal(int n);
+ void print_square(int size);
+ void print_triangle(int size);
+
+#endif/*MATH_H*/
