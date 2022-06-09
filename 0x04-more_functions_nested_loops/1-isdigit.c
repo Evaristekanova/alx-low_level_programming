4bd78451f6cb692e65bd1d1af21cf43b3692beb3
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-09 07:50:57 -0700

    multiplication

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,1 @@
-#include "main.h"
-/**
- * _isdigit-cheching if letter is digit
- *
- * @c:parameter for the function
- * Return:0 if success
- */
-int _isdigit(int c)
-{
-	if (c <= 48 && c >= 57)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul-function name to do multiplication
+ *
+ * @a:parameter 1
+ * @b:parameter 2
+ * Return:value
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
