2d0e5c4b8629d4c7aa92d04e079a1cf43efe8bf6
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-10 11:36:16 -0700

    skipping some numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,14 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 		{
 			continue;
 		}
-		_putchar(i % 10 +'0');
+		_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
 }
