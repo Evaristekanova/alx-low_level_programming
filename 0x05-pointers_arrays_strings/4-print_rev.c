7d7cdba6c541ec56f10a5fd029fe294c9347e8b6
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-13 13:29:30 -0700

    string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,19 @@ void print_rev(char *s)
 {
 	int i;
 	int n;
+	int counter;
 
-	for (i = 0; i >= n; i--)
+	counter = 0;
+	for (i = 0; i >= 0; i++)
 	{
 		if (s[i] == '\0')
 		{
 			break;
 		}
+		counter++;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
