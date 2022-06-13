d60ef0a70c7d58ed4144843f8828990f75854ed2
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-13 09:25:33 -0700

    updating

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - updating number to point to 98
+ *
+ * @n:argument1
+ *
+ * Return:void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MATH_H
+#define MATH_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /*MATH_H*/
