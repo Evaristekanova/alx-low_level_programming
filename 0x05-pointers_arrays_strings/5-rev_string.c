0c99b306b6e32d72735a4e725a159d3197aa5ea7
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-13 14:02:43 -0700

    reversing string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,17 +11,12 @@ void rev_string(char *s)
 	int i;
 	int counter;
 
-	counter = 0;
-	for (i = 0; i >= 0; i++)
+	for (i > 0; i >= 0; i--)
 	{
 		if (s[i] == '\0')
 		{
 			break;
 		}
-		counter++;
-	}
-	for (i >= counter - 1; i >= 0; i--)
-	{
 		_putchar(s[i]);
 	}
 }
