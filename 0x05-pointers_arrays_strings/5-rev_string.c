8a2fb3a5beeec4486b8698726132f12dc58668c6
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-13 23:33:36 -0700

    printing last half of string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,13 @@ void rev_string(char *s)
 	int i;
 	int counter;
 
-	for (i > 0; i >= 0; i--)
+	counter = 0;
+	for (i = 0; i >= 0; i--)
 	{
 		if (s[i] == '\0')
 		{
 			break;
 		}
-		_putchar(s[i]);
+		counter++;
 	}
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * puts_half - printingthe last half elements of array
+ *
+ *  @str: array arg
+ *
+ *  Return:void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int lenght = 0;
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
+		lenght++;
+	}
+	if (lenght % 2 != 0)
+	{
+		n = (lenght - 1) / 2;
+	}
+	else
+	{
+		n = lenght / 2;
+	}
+	for (i = n; i <= lenght; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
