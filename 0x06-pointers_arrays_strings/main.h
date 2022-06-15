46088ef25f7927d26f5ba6a63834097c70d60c8b
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-15 10:43:04 -0700

    reversing array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,17 +1,16 @@
-#ifndef MATH_H
-#define MATH_H
-/**
- * main.h - header file
- *
- * Return:0 if success
- */
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/* the entire header file file */
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
 char *leet(char *);
-int _putchar();
-#endif /*MAIN_H*/
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+#endif
