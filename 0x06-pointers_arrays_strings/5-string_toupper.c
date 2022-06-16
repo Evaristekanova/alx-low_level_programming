84daa1bdb7e045b0196f4c6e99ac580bbd9cde3c
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-16 01:39:04 -0700

    toupper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
  * string_toupper - uppercase
- *
+ *@s:para1
  * Return:value
  */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
-	if (*ch >= 97 || ch <= 122)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+		_putchar(s[i]);
+	}
+}
 
