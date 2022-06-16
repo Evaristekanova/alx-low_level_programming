88c2403715b312bfd5c9136c1da9911357083e87
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-15 23:44:19 -0700

    capitalize every word of string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,34 @@
 /**
  * cap_string - capitalizing all words of a string
  *
+ * @s:para1
  * Return:void
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				_putchar(s[i]);
+			}
+		}
+		else if (s[i] == ' ')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+				_putchar(s[i]);
+			}
+		}
+		else
+		{
+			_putchar(s[i]);
+		}
+	}
 }
