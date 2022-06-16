2e79e38acd266b493d10501d29666c1020d501f6
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-16 11:39:28 -0700

    print array of integer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,15 +18,27 @@ void reverse_array(int *a, int n)
 	}
 	for (count = n - 1; count >= 0; count--)
 	{
-		if (a[count] % 10 != 0)
+		if (count != 0 )
 		{
-			_putchar(a[count] / 10 + '0');
+			if (a[count] % 10 != 0)
+			{
+				_putchar(a[count] / 10);
+			}
+			else
+			{
+				_putchar(a[count] / 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		else
 		{
-			_putchar(a[count] / 10 + '0');
+			if (n1 / 10 != 0)
+			{
+				print_number(n1 / 10);
+			}
+			_putchar((n1 % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
-	}
 }
