f2beafa7ec459f568d6fa8c813a87ec4746a0505
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-25 01:59:34 -0700

    create library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/* the entire header file file */
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN.H*/
+#endif
