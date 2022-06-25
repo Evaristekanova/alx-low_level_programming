093ba9f65f56306e6bdf810bebf3a730923bd400
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-25 03:27:42 -0700

    displaying program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - display the name of program
+ * @argc: argument counter
+ * @argv:array of string
+ * Return:0 if success
+ */
+int main(int  __attribute__ ((unused)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
