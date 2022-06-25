e67120a9e98d7c259fdc1cb2e3adc19ce1d4f047
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-25 03:16:34 -0700

    returning number of args on cmd line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - writes the character c to stdout
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return:0 if success
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int main(int argc, char **argv)
+{
+	argv = argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
