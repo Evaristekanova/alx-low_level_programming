cfb527c50853abdd22985f9f064fe4f69b17ba92
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-25 05:15:30 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,12 @@ int main(int argc, char **argv)
 
 	if (argc < 3)
 	{
-		printf("Error\n");
+		printf("Error");
+		printf("\n");
 		return (1);
 	}
 	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", mul);
+	printf("%d", mul);
+	printf("\n");
 	return (0);
 }
