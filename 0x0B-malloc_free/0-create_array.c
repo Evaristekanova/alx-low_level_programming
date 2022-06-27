72b89f952a76e1574483657de0a1b3472fa14758
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-27 11:41:16 -0700

    create array of a string

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - creating array of a string and initialize it
+ *
+ * @size:size of array
+ * @c:single character to the areray
+ *
+ * Return:value
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(size * sizeof(c));
+
+	if (array == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
