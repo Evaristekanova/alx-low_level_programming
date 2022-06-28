acd2da743dee48d4e8d98ed90c9f2b10739356b1
Evaristekanova <dusingizimanaevariste3@gmail.com>
2022-06-28 03:06:43 -0700

    malloc project

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *strout;
+	unsigned int i, j, k, limit;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+
+
